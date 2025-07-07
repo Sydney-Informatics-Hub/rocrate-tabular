c9cba23c482d52ee3d8b6b77a3f0dda728155420
Mike Lynch <m.lynch@sydney.edu.au>
2025-07-07 17:08:14 +1000

    Add some actual testing for expand_props - doesn't work yet

--- tests/test_expanded_properties.py
+++ tests/test_expanded_properties.py
@@ -1,6 +1,18 @@
-import pytest
+from util import tabulator, read_config, write_config
+from pathlib import Path
+import sys
 
 
-@pytest.mark.skip("Not implemented")
-def test_expanded_properties(tmp_path):
-    assert True
+def test_expanded_properties(crates, tmp_path):
+    tb = tabulator(tmp_path, crates["languageFamily"])
+    cffile = Path(tmp_path) / "config.json"
+    cf = read_config(cffile)
+    cf["tables"]["RepositoryObject"]["expand_props"] = ["author", "publisher"]
+
+    write_config(cf, cffile)
+    tb.load_config(cffile)
+    tb.entity_table("RepositoryObject")
+    rows = list(tb.db.query("SELECT * FROM RepositoryObject"))
+    assert len(rows) > 1
+    for row in rows:
+        print(row, file=sys.stderr)
